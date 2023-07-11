bd959dab41bfe67e91b7b12d49777d89d878128e
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-07-11 11:36:20 +0800

    add discord icon

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,18 +1,19 @@
 import React from 'react'
-import { BsPlus, BsFillLightningFill, BsGearFill} from 'react-icons/bs'
+import { BsPlus, BsCompassFill, BsDiscord } from 'react-icons/bs'
 import { FaFire, FaPoo} from 'react-icons/fa'
 
 
 const SideBar = () => {
     return (
         <div className='fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow'>
-        <SideBarIcon icon={<FaFire size={28} />} />
-        <SideBarIcon icon={<BsPlus size={32} />} />
+            <SideBarIcon icon={<BsDiscord size={28} />} text='Direct Messages' />
+            <SideBarIcon icon={<BsPlus size={32} />} text='Add a Server' />
+            <SideBarIcon icon={<BsCompassFill size={24} />} text='Explore Discoverable Servers' />
         </div>
     )
 }
 
-const SideBarIcon = ({icon, text = 'tooltip'}) => {
+const SideBarIcon = ({icon, text}) => {
     return (
         <div className='sidebar-icon group'>
             {icon}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   mode: 'jit',
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
