3adcd0ae5bf2dba883f0e6e4c39701f0d91d1f2a
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-07-11 17:04:05 +0800

    update sidebar icon to sidebar link

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 import { BsPlus, BsCompassFill, BsDiscord, BsDownload } from 'react-icons/bs'
 
@@ -5,12 +6,12 @@ import { BsPlus, BsCompassFill, BsDiscord, BsDownload } from 'react-icons/bs'
 const SideBar = () => {
     return (
         <div className='fixed top-0 left-0 h-screen w-16 m-0 flex flex-col align-middle items-center gap-2 bg-gray-900 text-white shadow py-2'>
-            <SideBarLogo icon={<BsDiscord size={28} />} text='Direct Messages' />
-            <hr className='w-[50%] mx-auto bg-gray-800 text-gray-800 h-[2px] border-0 rounder-sm'/>
-            <SideBarIcon icon={<BsPlus size={32} />} text='Add a Server' />
-            <SideBarIcon icon={<BsCompassFill size={22} />} text='Explore Discoverable Servers' />
-            <hr className='w-[50%] mx-auto bg-gray-800 text-gray-800 h-[2px] border-0 rounder-sm' />
-            <SideBarIcon icon={<BsDownload size={20} />} text='Download Apps' />
+            <SideBarLogo icon={<BsDiscord size={28} />} text='Direct Messages' href="#"/>
+            <hr className='w-[50%] mx-auto bg-gray-800 text-gray-800 h-[2px] border-0 rounder-sm'href="#"/>
+            <SideBarIcon icon={<BsPlus size={32} />} text='Add a Server' href="#"/>
+            <SideBarIcon icon={<BsCompassFill size={22} />} text='Explore Discoverable Servers' href="#"/>
+            <hr className='w-[50%] mx-auto bg-gray-800 text-gray-800 h-[2px] border-0 rounder-sm' href="#"/>
+            <SideBarIcon icon={<BsDownload size={20} />} text='Download Apps' href="#" />
         </div>
     )
 }
@@ -26,14 +27,14 @@ const SideBarLogo = ({ icon, text }) => {
     )
 }
 
-const SideBarIcon = ({icon, text}) => {
+const SideBarIcon = ({icon, text, href}) => {
     return (
-        <div className='sidebar-icon group'>
+        <Link className='sidebar-icon group' href={href}>
             {icon}
             <span className='sidebar-tooltip group-hover:scale-100'>
                 {text}
             </span>
-        </div>
+        </Link>
     )
 }
 
