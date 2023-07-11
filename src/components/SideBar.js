99a3d996de49419042295c0284b11964f893ed06
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-07-11 11:05:13 +0800

    add tooltip

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,13 +1,24 @@
 import React from 'react'
+import { BsPlus, BsFillLightningFill, BsGearFill} from 'react-icons/bs'
+import { FaFire, FaPoo} from 'react-icons/fa'
+
 
 const SideBar = () => {
     return (
         <div className='fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow'>
-            <i>A</i>
-            <i>B</i>
-            <i>C</i>
-            <i>D</i>
-            <i>E</i>
+        <SideBarIcon icon={<FaFire size={28} />} />
+        <SideBarIcon icon={<BsPlus size={32} />} />
+        </div>
+    )
+}
+
+const SideBarIcon = ({icon, text = 'tooltip'}) => {
+    return (
+        <div className='sidebar-icon'>
+            {icon}
+            <span className='sidebar-tooltip'>
+                {text}
+            </span>
         </div>
     )
 }
