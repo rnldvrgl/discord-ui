ccadbb7399379b1b7a7f7288c6a8adf7eee51654
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-07-11 15:00:52 +0800

    add discover button

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,17 @@
-import SideBar from '@/components/SideBar'
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <main className="flex">
-      <SideBar />
+    <main className="flex h-screen w-full ps-[4rem] pe-3">
+      <div className="flex-col w-[25%] bg-[#2B2D31] p-3">
+        <button className="px-2 py-2 bg-gray-900 text-gray-400 text-xs w-full text-left rounded-sm">
+          Find or start a conversation
+        </button>
+        <div>
+          <div></div>
+        </div>
+      </div>
+      <div className="">
+
+      </div>
     </main>
   )
 }
