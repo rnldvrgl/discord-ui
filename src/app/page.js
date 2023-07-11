1795e33c6c952e2f2c719e61aa31302a05af7bab
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-07-11 16:08:23 +0800

    add Horizontal Dark component

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,22 @@
+import HorizontalDark from "@/components/HorizontalDark";
+
 export default function Home() {
   return (
-    <main className="flex h-screen w-full ps-[4rem] pe-3">
-      <div className="flex-col w-[25%] bg-[#2B2D31] p-3">
-        <button className="px-2 py-2 bg-gray-900 text-gray-400 text-xs w-full text-left rounded-sm">
-          Find or start a conversation
-        </button>
-        <div>
-          <div></div>
+    <section className="flex items-start h-full w-full ps-[4rem]">
+      <div className="flex-col w-[25%]">
+        <div className="p-2 bg-[#2B2D31]">
+          <button className="bg-gray-900 text-gray-400 text-xs w-full text-left rounded-sm p-2">
+            Find or start a conversation
+          </button>
+        </div>
+        <HorizontalDark />
+        <div className="p-2 h-full bg-[#2B2D31]">
+          
         </div>
       </div>
-      <div className="">
+      <div className="flex-col w-[75%] h-screen bg-[#313338]">
 
       </div>
-    </main>
+    </section>
   )
 }
---
+++ src/components/HorizontalDark.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HorizontalDark = () => {
+  return (
+      <hr className='w-full bg-gray-900 h-[2px] border-0 rounder-sm' />
+  )
+}
+
+export default HorizontalDark
