b8a38f343e05a68b9fdccbc8e67455b9be5b2989
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-07-12 11:34:53 +0800

    add cross button to contacts

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import HorizontalDark from "@/components/HorizontalDark";
-import { BsPlus } from 'react-icons/bs'
+import { BsPlus} from 'react-icons/bs'
+import { RxCross2 } from 'react-icons/rx'
 import { FriendsIcon, NitroIcon, MessageReqIcon } from "@/components/Icons";
 import Image from "next/image";
 Image
@@ -19,7 +20,7 @@ export default function Home() {
             <CustomButton icon={<NitroIcon size={22} />} text="Nitro" />
             <CustomButton icon={<MessageReqIcon size={22} />} text="Message Requests" />
           <Category icon={<BsPlus size={22} />} text="Direct Messages" />
-<div className="flex flex-col gap-4">
+<div className="flex flex-col gap-1">
             <Contacts image="artist.png" name="Mark Artist" />
             <Contacts image="coach.png" name="Mark Coach" />
             <Contacts image="cook.png" name="Mark Cook" />
@@ -59,15 +60,18 @@ const Category = ({ icon, text }) => {
 const Contacts = ({ image, name}) => {
   const imagePath = `/assets/static/avatar/${image}`;
   return(
-    <div className="px-2 flex justify-start items-center gap-3 wi-full text-white">
-      <div className="bg-white rounded-full px-2 py-1 relative">
-        <Image src={imagePath} alt={name} width={25} height={25}/>
-        <div className="border-[3px] border-[#7B7E88] bg-[#323338] h-3 w-3  rounded-full absolute bottom-[-1px] right-0 z-10"></div>
-        <div className="bg-[#323338] h-5 w-5 rounded-full absolute bottom-[-5px] right-[-4px] "></div>
-</div>
-      <span className="text-sm">
-        {name}
-      </span>
+    <div className="px-2 flex justify-between items-center gap-3 w-full text-white group transition-all duration-200 ease-linear cursor-pointer hover:text-gray-400 hover:bg-[#35373C] py-2 rounded-lg">
+      <div className="flex justify-center items-center gap-3">
+        <div className="bg-white rounded-full px-2 py-1 relative">
+          <Image src={imagePath} alt={name} width={25} height={25} />
+          <div className="border-[3px] border-[#7B7E88] bg-[#323338] h-3 w-3  rounded-full absolute bottom-[-1px] right-0 z-10"></div>
+          <div className="bg-[#323338] h-5 w-5 rounded-full absolute bottom-[-5px] right-[-4px] "></div>
+        </div>
+        <span className="text-sm">
+          {name}
+        </span>
+      </div>
+      <RxCross2 className="group-hover:scale-100 transition-all duration-100 scale-0"/>
     </div>
   )
 }
