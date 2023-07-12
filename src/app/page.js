f0f44722920b7e823eb9a907c75e9ab71c1c2f16
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-07-12 10:28:22 +0800

    update page height

--- src/app/page.js
+++ src/app/page.js
@@ -4,25 +4,25 @@ import { FriendsIcon, NitroIcon, MessageReqIcon } from "@/components/Icons";
 
 export default function Home() {
   return (
-    <section className="flex items-start h-full w-full ps-[4rem]">
-      <div className="flex-col w-[30%]">
-        <div className="p-2 bg-[#2B2D31]">
-          <button className="bg-gray-900 text-gray-400 text-xs w-full text-left rounded-sm p-2">
-            Find or start a conversation
-          </button>
+    <section className="flex items-start h-screen w-full ps-[4rem]">
+      <div className="flex-col w-[30%]  bg-[#2B2D31] h-full">
+          <div className="p-2">
+            <button className="bg-gray-900 text-gray-400 text-xs w-full text-left rounded-sm p-2">
+              Find or start a conversation
+            </button>
+          </div>
+          <HorizontalDark />
+          <div className="p-2 flex-grow">
+            <CustomButton icon={<FriendsIcon />} text="Friends" />
+            <CustomButton icon={<NitroIcon size={22} />} text="Nitro" />
+            <CustomButton icon={<MessageReqIcon size={22} />} text="Message Requests" />
+            <Category icon={<BsPlus size={22} />} text="Direct Messages" />
+            <Contacts icon={<BsPlus size={22} />} name="Direct Messages" />
         </div>
-        <HorizontalDark />
-        <div className="p-2 h-full bg-[#2B2D31]">
-          <CustomButton icon={<FriendsIcon />} text="Friends" />
-          <CustomButton icon={<NitroIcon size={22} />} text="Nitro" />
-          <CustomButton icon={<MessageReqIcon size={22} />} text="Message Requests" />
-          <Category icon={<BsPlus size={22} />} text="Direct Messages" />
 
-          <Contacts icon={<BsPlus size={22} />} name="Direct Messages" />
-        </div>
       </div>
-      <div className="flex-col w-[70%] h-screen bg-[#313338]">
-
+      <div className="flex-col w-[70%] h-full bg-[#313338]">
+        {/* Rest of the content */}
       </div>
     </section>
   )
